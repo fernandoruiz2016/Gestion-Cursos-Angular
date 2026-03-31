0c820581c62dec20217759112537e8d6088454bc
Fernando Ruiz <fernandoruiz696@gmail.com>
2026-03-31 10:26:06 -0500

    Loading corregido

--- backend/apis/dashboard/dashboard.service.js
+++
@@ -1,9 +0,0 @@
-const dashboardRepository = require("./dashboard.repository");
-
-async function obtenerDashboard() {
-  return await dashboardRepository.obtenerResumenDashboard();
-}
-
-module.exports = {
-  obtenerDashboard,
-};
--- backend/apis/dashboard/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const dashboardController = require("./dashboard.controller");
-
-router.get("/", dashboardController.obtenerDashboard);
-
-module.exports = router;
